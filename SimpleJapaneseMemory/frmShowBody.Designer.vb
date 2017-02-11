5476a76a1faeff103d43f7d2fc1958dcca0ffae1
tonywang806 <tonywang806@gmail.com>
2017-02-11 11:54:58 +0900

    [change]からだの読み方をテストできるように実装しました。

--- SimpleJapaneseMemory/frmShowBody.Designer.vb
+++ SimpleJapaneseMemory/frmShowBody.Designer.vb
@@ -24,7 +24,23 @@ Partial Class frmShowBody
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmShowBody))
         Me.pnlBody = New System.Windows.Forms.Panel()
+        Me.Panel15 = New System.Windows.Forms.Panel()
+        Me.Panel14 = New System.Windows.Forms.Panel()
+        Me.Panel13 = New System.Windows.Forms.Panel()
+        Me.Panel12 = New System.Windows.Forms.Panel()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.btnClose = New System.Windows.Forms.Button()
+        Me.pnlBody.SuspendLayout()
         Me.SuspendLayout()
         '
         'pnlBody
@@ -35,10 +51,145 @@ Partial Class frmShowBody
         Me.pnlBody.BackColor = System.Drawing.Color.Transparent
         Me.pnlBody.BackgroundImage = Global.SimpleJapaneseMemory.My.Resources.Resources.BodyAndPronouce
         Me.pnlBody.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.pnlBody.Controls.Add(Me.Panel15)
+        Me.pnlBody.Controls.Add(Me.Panel14)
+        Me.pnlBody.Controls.Add(Me.Panel13)
+        Me.pnlBody.Controls.Add(Me.Panel12)
+        Me.pnlBody.Controls.Add(Me.Panel11)
+        Me.pnlBody.Controls.Add(Me.Panel10)
+        Me.pnlBody.Controls.Add(Me.Panel9)
+        Me.pnlBody.Controls.Add(Me.Panel8)
+        Me.pnlBody.Controls.Add(Me.Panel7)
+        Me.pnlBody.Controls.Add(Me.Panel6)
+        Me.pnlBody.Controls.Add(Me.Panel5)
+        Me.pnlBody.Controls.Add(Me.Panel4)
+        Me.pnlBody.Controls.Add(Me.Panel3)
+        Me.pnlBody.Controls.Add(Me.Panel2)
+        Me.pnlBody.Controls.Add(Me.Panel1)
         Me.pnlBody.Location = New System.Drawing.Point(301, 73)
         Me.pnlBody.Name = "pnlBody"
         Me.pnlBody.Size = New System.Drawing.Size(742, 544)
         Me.pnlBody.TabIndex = 0
+        '
+        'Panel15
+        '
+        Me.Panel15.BackColor = System.Drawing.Color.White
+        Me.Panel15.Location = New System.Drawing.Point(634, 503)
+        Me.Panel15.Name = "Panel15"
+        Me.Panel15.Size = New System.Drawing.Size(90, 31)
+        Me.Panel15.TabIndex = 0
+        '
+        'Panel14
+        '
+        Me.Panel14.BackColor = System.Drawing.Color.White
+        Me.Panel14.Location = New System.Drawing.Point(634, 440)
+        Me.Panel14.Name = "Panel14"
+        Me.Panel14.Size = New System.Drawing.Size(90, 31)
+        Me.Panel14.TabIndex = 0
+        '
+        'Panel13
+        '
+        Me.Panel13.BackColor = System.Drawing.Color.White
+        Me.Panel13.Location = New System.Drawing.Point(634, 376)
+        Me.Panel13.Name = "Panel13"
+        Me.Panel13.Size = New System.Drawing.Size(90, 31)
+        Me.Panel13.TabIndex = 0
+        '
+        'Panel12
+        '
+        Me.Panel12.BackColor = System.Drawing.Color.White
+        Me.Panel12.Location = New System.Drawing.Point(496, 238)
+        Me.Panel12.Name = "Panel12"
+        Me.Panel12.Size = New System.Drawing.Size(87, 31)
+        Me.Panel12.TabIndex = 0
+        '
+        'Panel11
+        '
+        Me.Panel11.BackColor = System.Drawing.Color.White
+        Me.Panel11.Location = New System.Drawing.Point(496, 181)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(87, 31)
+        Me.Panel11.TabIndex = 0
+        '
+        'Panel10
+        '
+        Me.Panel10.BackColor = System.Drawing.Color.White
+        Me.Panel10.Location = New System.Drawing.Point(496, 125)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(87, 31)
+        Me.Panel10.TabIndex = 0
+        '
+        'Panel9
+        '
+        Me.Panel9.BackColor = System.Drawing.Color.White
+        Me.Panel9.Location = New System.Drawing.Point(496, 70)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(87, 31)
+        Me.Panel9.TabIndex = 0
+        '
+        'Panel8
+        '
+        Me.Panel8.BackColor = System.Drawing.Color.White
+        Me.Panel8.Location = New System.Drawing.Point(16, 471)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(91, 31)
+        Me.Panel8.TabIndex = 0
+        '
+        'Panel7
+        '
+        Me.Panel7.BackColor = System.Drawing.Color.White
+        Me.Panel7.Location = New System.Drawing.Point(16, 417)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(91, 31)
+        Me.Panel7.TabIndex = 0
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.White
+        Me.Panel6.Location = New System.Drawing.Point(16, 361)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(91, 31)
+        Me.Panel6.TabIndex = 0
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.White
+        Me.Panel5.Location = New System.Drawing.Point(104, 299)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(89, 31)
+        Me.Panel5.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.White
+        Me.Panel4.Location = New System.Drawing.Point(104, 247)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(89, 31)
+        Me.Panel4.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Location = New System.Drawing.Point(104, 193)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(89, 31)
+        Me.Panel3.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.Location = New System.Drawing.Point(104, 138)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(89, 31)
+        Me.Panel2.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Location = New System.Drawing.Point(104, 81)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(89, 31)
+        Me.Panel1.TabIndex = 0
         '
         'btnClose
         '
@@ -58,8 +209,7 @@ Partial Class frmShowBody
         '
         'frmShowBody
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.BackgroundImage = Global.SimpleJapaneseMemory.My.Resources.Resources.BlackBoard
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(1354, 721)
@@ -72,10 +222,26 @@ Partial Class frmShowBody
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "frmShowBody"
         Me.TransparencyKey = System.Drawing.SystemColors.Control
+        Me.pnlBody.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents pnlBody As Panel
     Friend WithEvents btnClose As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents Panel14 As Panel
+    Friend WithEvents Panel13 As Panel
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
 End Class
--- SimpleJapaneseMemory/frmShowBody.vb
+++ SimpleJapaneseMemory/frmShowBody.vb
@@ -2,4 +2,12 @@
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Me.Close()
     End Sub
+
+    Private Sub Panel_MouseEnter(sender As Object, e As EventArgs) Handles Panel9.MouseEnter, Panel8.MouseEnter, Panel7.MouseEnter, Panel6.MouseEnter, Panel5.MouseEnter, Panel4.MouseEnter, Panel3.MouseEnter, Panel2.MouseEnter, Panel15.MouseEnter, Panel14.MouseEnter, Panel13.MouseEnter, Panel12.MouseEnter, Panel11.MouseEnter, Panel10.MouseEnter, Panel1.MouseEnter
+        CType(sender, Panel).BackColor = Color.Transparent
+    End Sub
+
+    Private Sub Panel_MouseLeave(sender As Object, e As EventArgs) Handles Panel9.MouseLeave, Panel8.MouseLeave, Panel7.MouseLeave, Panel6.MouseLeave, Panel5.MouseLeave, Panel4.MouseLeave, Panel3.MouseLeave, Panel2.MouseLeave, Panel15.MouseLeave, Panel14.MouseLeave, Panel13.MouseLeave, Panel12.MouseLeave, Panel11.MouseLeave, Panel10.MouseLeave, Panel1.MouseLeave
+        CType(sender, Panel).BackColor = Color.White
+    End Sub
 End Class
